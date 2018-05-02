e68f83467cc38badec62f94c59f9b6e54318c50d
Pablo Escobar <pablo.escobarlopez@unibas.ch>
2018-05-02 21:55:15 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# make sure that gcc is installed
+
 INSTALL_DIR="$HOME/.terraform-ansible-tests"
 
 curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
@@ -7,10 +9,9 @@ bash Miniconda2-latest-Linux-x86_64.sh -p ${INSTALL_DIR} -b
 export PATH=${INSTALL_DIR}/bin:$PATH
 conda update -y -n base conda
 pip install --upgrade pip
-conda install --yes -c conda-forge ansible
+pip install ansible
+pip install python-openstackclient
 conda install --yes -c conda-forge terraform
-conda install -y wget
-conda install -y -c hargup/label/pypi python-openstackclient
 # ansible dynamic inventory to read terraform state files
 curl -o ${INSTALL_DIR}/bin/terraform.py https://raw.githubusercontent.com/CiscoCloud/k8sclass/master/02-Install/Terraform/terraform.py
 chmod +x ${INSTALL_DIR}/bin/terraform.py
